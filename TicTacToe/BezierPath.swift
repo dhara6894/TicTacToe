d8821f5d94732913f4f13b84f2027706a9831dbf
Dhara Patel <dharapatel@Dharas-Mac-mini.local>
2018-08-02 23:48:04 +0530

    added stroke on finish

---
+++ TicTacToe/BezierPath.swift
@@ -0,0 +1,12 @@
+//
+//  BezierPath.swift
+//  TicTacToe
+//
+//  Created by Dhara Patel on 02/08/18.
+//  Copyright © 2018 SASA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
--- TicTacToe/ZeroxCollectionViewController.swift
+++ TicTacToe/ZeroxCollectionViewController.swift
@@ -13,6 +13,7 @@ class ZeroxCollectionViewController: UIViewController {
     var count = 0
     var playerOneName = String()
     var playerTwoName = String()
+    var linePosition = [CGPoint]()
     var arrCross = [Int]()
     var arrZero =  [Int]()
     let arrPosibileValue = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
@@ -30,6 +31,8 @@ class ZeroxCollectionViewController: UIViewController {
         self.navigationController?.navigationBar.isHidden = true
         self.automaticallyAdjustsScrollViewInsets = false
         IBlblDesision.isHidden = true
+        
+      
 //        IBlblPlayerOne.text = playerOneName
 //        IBlblPlayerTwo.text = playerTwoName
 
@@ -37,6 +40,36 @@ class ZeroxCollectionViewController: UIViewController {
     private func setUpCollectionView(){
         zeroXCollectionView.allowsMultipleSelection = true
         zeroXCollectionView.register(UINib(nibName: "CollectionViewCell" , bundle: nil), forCellWithReuseIdentifier: "collectionCell")
+        zeroXCollectionView.layer.cornerRadius = 15.0
+        zeroXCollectionView.clipsToBounds = true
+        addShadow(to: zeroXCollectionView.layer, with: 5.0)
+        zeroXCollectionView.layer.masksToBounds = false
+    }
+    private func addLine(from startPoint: CGPoint, to endPoint: CGPoint, collView:UIView){
+        // create path
+        let path = UIBezierPath()
+        path.move(to: startPoint)
+        path.addLine(to: endPoint)
+
+        // Create a `CAShapeLayer` that uses that `UIBezierPath`:
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = UIColor.green.cgColor
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = 12
+        shapeLayer.lineJoin = kCALineJoinBevel
+        addShadow(to: shapeLayer, with: 3.0)
+        
+        // Add that `CAShapeLayer` to your view's layer:
+        UIView.animate(withDuration: 1, animations: {
+            self.view.layer.addSublayer(shapeLayer)
+        })
+        
+        let pathAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        pathAnimation.duration = 0.3
+        pathAnimation.fromValue = 0
+        pathAnimation.toValue = 1
+        shapeLayer.add(pathAnimation, forKey: "strokeEnd")
     }
 }
 extension ZeroxCollectionViewController:  UICollectionViewDelegateFlowLayout{
@@ -48,6 +81,7 @@ extension ZeroxCollectionViewController:  UICollectionViewDelegateFlowLayout{
             + flowLayout.sectionInset.right
             + (flowLayout.minimumInteritemSpacing * CGFloat(2))
         let size = Int((zeroXCollectionView.bounds.size.width - totalSpace) / CGFloat(3))
+        
         return CGSize(width: size, height: size)
         
     }
@@ -67,12 +101,15 @@ extension ZeroxCollectionViewController{
                 sCrossOrZeroLost = Set(arrCross)
                 message = "\(playerOneName) Won"
             }
+            
             //check arrayValue elements contain in cross array or zero array
             if sPosibilitiesList.isSubset(of: sCrossOrZeroLost){
                 for j in i{                                      //if match with posibilities than color it
                     let indexPath = IndexPath(item: j, section: 0)
-                    let cell =  zeroXCollectionView.cellForItem(at: indexPath as IndexPath) as? CollectionViewCell
-                    cell?.backgroundColor = UIColor.green
+                    
+                    guard let cell =  zeroXCollectionView.cellForItem(at: indexPath as IndexPath) as? CollectionViewCell else { return }
+                   getStartEndPoint(from: indexPath)
+                    //cell.backgroundColor = UIColor.green
                     zeroXCollectionView.deselectItem(at: indexPath, animated: false)
                 }
                 IBlblDesision.isHidden = false
@@ -86,7 +123,18 @@ extension ZeroxCollectionViewController{
             }
         }
     }
-    
+    private func getStartEndPoint(from indexpath: IndexPath){
+        let attributes: UICollectionViewLayoutAttributes? = zeroXCollectionView.layoutAttributesForItem(at: indexpath)
+        let cellRect: CGRect? = attributes?.frame
+        let cellFrameInSuperview = zeroXCollectionView.convert(cellRect ?? CGRect.zero, to: zeroXCollectionView.superview)
+        linePosition.append(CGPoint(x:cellFrameInSuperview.origin.x , y: cellFrameInSuperview.origin.y))
+        let width = cellFrameInSuperview.size.width
+        if linePosition.count == 3{
+            
+            addLine(from: CGPoint(x: linePosition[0].x + (width / 2) , y: linePosition[0].y + (width / 2)), to: CGPoint(x: linePosition[2].x + (width - (width / 2)), y: linePosition[2].y + (width - (width / 2))), collView: zeroXCollectionView)
+        }
+        
+    }
     @IBAction func btnBackClicked(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
@@ -107,13 +155,15 @@ extension ZeroxCollectionViewController{
      }
     private func addAnimationToImgView(img: UIImage,imgView: UIImageView){
         UIView.transition(with: imgView, duration: 0.5, options: .transitionCrossDissolve, animations: {
-          
-            imgView.layer.shadowColor = UIColor.gray.cgColor
-            imgView.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-            imgView.layer.shadowOpacity = 3.0
-            imgView.layer.shadowRadius = 1.0
+            self.addShadow(to: imgView.layer, with: 1.0)
             imgView.image = img
         }, completion: nil)
+    }
+    private func addShadow(to layer: CALayer, with raious : CGFloat){
+       layer.shadowColor = UIColor.gray.cgColor
+       layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+       layer.shadowOpacity = 3.0
+       layer.shadowRadius = raious
     }
 }
 extension ZeroxCollectionViewController: UICollectionViewDataSource{
@@ -143,6 +193,7 @@ extension ZeroxCollectionViewController: UICollectionViewDelegate{
                 arrCross.append(indexPath.row)
                 checkCrossValue()
             }
+
     }
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         count -= 1
